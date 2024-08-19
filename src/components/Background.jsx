d1f74acef2524595f7708d60bc179cbd355813f4
WillForEternity <112784638+WillForEternity@users.noreply.github.com>
2024-08-19 16:58:37 -0700

    Update Background.jsx

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -21,7 +21,7 @@ const Background = () => {
       const svg = svgRef.current;
       if (!svg) return;
 
-      time += 0.03;
+      time += 0.02;
 
       const paths = svg.querySelectorAll('path');
       paths.forEach((path) => {
